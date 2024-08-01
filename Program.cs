14bf1f838a17dc73f3a6563e7b41a7fab5265170
Matheus Cauã Polletti <matheus.c.polletti@gmail.com>
2024-08-01 14:47:25 -0300

    Atualização 3

--- Program.cs
+++ Program.cs
@@ -13,6 +13,7 @@ TextInfo transformarTexto = new CultureInfo("pt-br", false).TextInfo;
 
 while (nomeMago.Length < 7)
 {
+    Console.WriteLine("Por favor, grande mago, nos diga seu nome: ");
     nomeMago = transformarTexto.ToTitleCase(Convert.ToString(Console.ReadLine()));
     if (nomeMago.Length < 7)
     {
@@ -42,6 +43,7 @@ for (int c = 0; c < 3; c++)
         Console.WriteLine("O nome do feitiço não pode ser o mesmo que o nome do mago.");
         nomeFeiticoTemporario = transformarTexto.ToTitleCase(Convert.ToString(Console.ReadLine()));
     }
+
     feiticosTotais.Add(nomeFeiticoTemporario);
     nomeFeiticoTemporario = "";
 }
@@ -73,41 +75,56 @@ class modeloMago
         Contador++;
     }
 
-    public void ChamarFeitico()
+    public int ChamarFeitico()
     {
         if (espacosFeiticos > 0)
         {
             Console.WriteLine("Escolha qual feitiço você quer usar: \n1 - {0}\n2 - {1}\n3 - {2}", feitico[0], feitico[1], feitico[2]);
             int numeroFeitico = Convert.ToInt32(Console.ReadLine());
+            
             if (numeroFeitico > 3 || numeroFeitico < 1)
             {
                 Console.WriteLine("Esse feitiço não existe");
+                return 0;
             }
             else
             {
                 Console.WriteLine("O mago {0} usou o feitiço de {1}.", nome, feitico[numeroFeitico - 1]);
                 espacosFeiticos--;
                 Random aumento = new Random();
-                float aumentoExperiencia = aumento.Next(3, 7) / 10f;
+                float aumentoExperiencia = aumento.Next(1, 5) / 10f;
                 experiencia += aumentoExperiencia;
+                switch (numeroFeitico)
+                {
+                    case 1:
+                        return 40;
+                    case 2:
+                        return 20;
+                    case 3:
+                        return 10;
+                    default:
+                        break;
+                }
             }
         }
         else
         {
             Console.WriteLine("O mago {0} não tem espaços suficientes para usar um feitiço.", nome);
+            return 0;
         }
+        return 0;
     }
 
     public void Meditar()
     {
         Random numero = new Random();
         Console.WriteLine("O mago {0} meditou por {1} horas.", nome, numero.Next(3, 6));
-        espacosFeiticos = espacosFeiticos + numero.Next(1, 3);
+        espacosFeiticos = espacosFeiticos + numero.Next(2, 6);
     }
 
     public void Dados()
     {
-        Console.WriteLine("O mago {0} tem {1} pontos de experiência, ele possui {2} espaços de feitiços e {3} poções.", nome, experiencia, espacosFeiticos, pocao);
+        Console.WriteLine("O mago {0} tem {1} pontos de experiência, ele possui {2} espaços de feitiços, {3} poções e {4} pontos de vida.", nome, experiencia, espacosFeiticos, pocao, vida);
     }
 
     public void BeberPocao()
@@ -115,7 +132,7 @@ class modeloMago
         if (pocao > 0 && vida > 0)
         {
             Random vidaAumento = new Random();
-            int vidaMago = vidaAumento.Next(20, 50);
+            int vidaMago = vidaAumento.Next(20, 51);
             pocao--;
             vida += vidaMago;
             if (vida > 100)
@@ -138,33 +155,66 @@ class modeloMago
             Console.WriteLine("Algo não funcionou no uso da poção");
         }
     }
+
+    public void LutarInimigo()
+    {
+        ModeloInimigo inimigo = new ModeloInimigo();
+        int inimigoAtual = inimigo.InimigoVaiLutar();
+
+        string nomeInimigo = inimigo.nomesInimigosNivel1[inimigoAtual];
+        int vidaInimigo = inimigo.vidaInimigoNivel1[inimigoAtual];
+        int danoInimigo = inimigo.danoInimigoNivel1[inimigoAtual];
+
+        while (vidaInimigo > 0)
+        {
+            int dano = ChamarFeitico();
+            vidaInimigo -= dano;
+            Console.WriteLine("Vida do {0}: {1}.", nomeInimigo, vidaInimigo);
+            if (vidaInimigo <= 0)
+            {
+                Random aumento = new Random();
+                int quantiaEspacos = aumento.Next(0, 2);
+                int quantiaPocao = aumento.Next(0, 3);
+                float quantiaExperiencia = aumento.Next(6, 9) / 10f;
+
+                espacosFeiticos += quantiaEspacos;
+                pocao += quantiaPocao;
+                experiencia += quantiaExperiencia;
+
+                Console.WriteLine("O mago {0} derrotou um {1} e ganhou {2} espaços de feitiço, {3} poções e {4} de experiência", nome, nomeInimigo, quantiaEspacos, quantiaPocao, quantiaExperiencia);
+                break;
+            }
+            vida -= danoInimigo;
+            Console.WriteLine("Vida do mago {0}: {1}.", nome, vida);
+            if (vida <= 0)
+            {
+                Console.WriteLine("O mago {0} morreu na batalha contra um {1}", nome, nomeInimigo);
+                break;
+            }
+        }
+    }
 }
 
 
-class Inimigo
+class ModeloInimigo
 {
     public List<string> nomesInimigosNivel1;
     public List<int> vidaInimigoNivel1;
     public List<int> danoInimigoNivel1;
 
-    public List<string> nomesInimigosNivel2;
-    public List<int> vidaInimigoNivel2;
-    public List<int> danoInimigoNivel2;
-
-    public List<string> nomesInimigosNivel3;
-    public List<int> vidaInimigoNivel3;
-    public List<int> danoInimigoNivel3;
-
-    public List<string> nomesInimigosNivel4;
-    public List<int> vidaInimigoNivel4;
-    public List<int> danoInimigoNivel4;
-
-    public static int InimigosDerrotados;
-
-    public Inimigo()
+    public ModeloInimigo()
     {
-        nomesInimigosNivel1 = ["Porco Espinho", "Rato Atroz",  "Escorpião", "Goblin"];
+        nomesInimigosNivel1 = ["Porco Espinho", "Rato Atroz", "Escorpião", "Goblin"];
         vidaInimigoNivel1 = [20, 30, 40, 60];
-        danoInimigoNivel1 = []
+        danoInimigoNivel1 = [12, 16, 20, 24];
+    }
+
+    public int InimigoVaiLutar()
+    {
+        Random gerarNumero = new Random();
+        int numeroInimigo = gerarNumero.Next(0, 4);
+
+        Console.WriteLine("Um {0} apareceu.\nVida: {1}\nDano: {2}", nomesInimigosNivel1[numeroInimigo], vidaInimigoNivel1[numeroInimigo], danoInimigoNivel1[numeroInimigo]);
+        return numeroInimigo;
     }
 }
--- obj/Debug/net8.0/rpg.AssemblyInfo.cs
+++ obj/Debug/net8.0/rpg.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("rpg")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0169a576bb850916e2baf18241a32ff321b7bfd9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24eaa7fca0bb9d8fbf426a57601798a6dddd4fe6")]
 [assembly: System.Reflection.AssemblyProductAttribute("rpg")]
 [assembly: System.Reflection.AssemblyTitleAttribute("rpg")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
